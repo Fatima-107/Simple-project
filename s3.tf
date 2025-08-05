8819d1995b6d30fe930a96abf97e4142215adacd
Fatima-107 <fatima107.alphasquad@gmail.com>
2025-08-05 20:01:58 +0500

    updated s3.tf file

--- s3.tf
+++ s3.tf
@@ -31,7 +31,10 @@ resource "aws_s3_bucket_policy" "public_policy" {
         Sid       = "PublicReadGetObject",
         Effect    = "Allow",
         Principal = "*",
-        Action    = "s3:GetObject",
+        Action    = [
+          "s3:GetObject",
+          "s3:PutBucketPolicy"
+        ]
         Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
       }
     ]
